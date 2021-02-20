bc29422d51a7082b99d90f5b43394ea7a3215f7d
picostar <tom@picostar.com>
2021-02-20 11:22:10 -0500

    doit

--- digi.py
+++ digi.py
@@ -5,13 +5,25 @@ from digidevice import runt
 
 runt.start() 
 
+def tree_traverse(tree):
+    for k in tree.items():
+          v = runt.get("k")
+          if v is not None:
+            return k,v
+
 def main():
 
+     digikeys = runt.keys("")
 
-    digikeys = runt.keys("")
+     for i in digikeys:
+          print (i)
+          mk = runt.keys(i)
+          for x in mk:
+               print (x)
+               y = runt.get("x")
+               print (y)
 
-    for i in digikeys:
-         print (i)
+     print (tree_traverse(digikeys))
 
 if __name__ == '__main__':
     main()
